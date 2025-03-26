28eb4dbaeecefb2bf76c038a9ed1b4842e0bcdc3
Edmundo Lacerda <elacerda@friese.equipamentos.local>
2025-03-26 08:58:25 -0300

    irrigacao

--- Irrigacao.cpp
+++ Irrigacao.cpp
@@ -1,16 +1,25 @@
 #include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-LiquidCrystal_I2C lcd(0x20, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Declarando variaveis dos sensores e seus tipos
 int umidade = 0;
-float temperatura = 0;
+#define dados 4
+
+int seco = 650;  // Variaveis para condicional 
+int molhado = 300; 
+
+OneWire OneWire(dados);
+DallasTemperature sensors(&OneWire);
 
 // Configurando arquitetura do sistema
 void setup() {
+
   pinMode(A0, OUTPUT);
   pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
+  pinMode(6, INPUT);
   pinMode(7, OUTPUT);
   
   // Iniciando o LCD 
@@ -19,57 +28,59 @@ void setup() {
   lcd.backlight();
 
   Serial.begin(9600);
+  sensors.begin();
 }
 
 void loop() {
   digitalWrite(A0, HIGH); 
   delay(10);
 
-  
-  int leitura = analogRead(A2); // Leitura da temperatura no pino A2
-  
-  float tensao = leitura * 5.0; // Converte a leitura para tensão
-  tensao /= 1024.0; 
-  
-  temperatura = (tensao - 0.5) * 100; // Converte em °C
+  int leitura = 0;
 
-  // Média de 5 leituras para reduzir ruído
+  sensors.requestTemperatures();
+  float temperatura = sensors.getTempCByIndex(0);
+  //Média de 3 leituras para reduzir ruído
   int umidadeSum = 0;
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 3; i++) {
     umidadeSum += analogRead(A1);
     delay(100); 
   }
 
-  umidade = umidadeSum / 5;
-  float umidadePor = (umidade / 1023.0) * 100.0;
+  leitura = umidadeSum / 3;
+
+  // leitura == seco; 0; leitura == molhado; 100;
+  int umidade = map(leitura, seco, molhado, 0, 100);
+
+  // Limita o valor de 0 a 100
+  umidade = constrain(umidade, 0, 100);
 
   // Exibe no monitor serial
   Serial.print("Umidade: ");
-  Serial.print(umidadePor);
+  Serial.print(umidade);
   Serial.print("% | Temperatura: ");
-  Serial.print(temperatura, 1);
+  Serial.print(temperatura);
   Serial.println("°C");
 
   // Exibe no LCD
   lcd.setCursor(0, 0); 
   lcd.print("Umidade: ");
-  lcd.print(umidadePor, 2);
+  lcd.setCursor(9, 0); 
+  lcd.print(umidade);
   lcd.print("%");
 
   lcd.setCursor(0, 1);
   lcd.print("Temp: ");
-  lcd.print(temperatura, 2);
+  lcd.print(temperatura);
   lcd.print(" C");
 
   // Ativa bomba se a umidade for baixa e a temperatura estiver alta
-  if (umidadePor < 30 && temperatura > 23) {
-    digitalWrite(7, HIGH);
+  if (umidade < 40 && temperatura > 23) {
+    digitalWrite(13, !HIGH);
     Serial.println("Ativando bomba!");
     delay(5000); // Mantém a bomba ligada por 5 segundos
   } else {
-    digitalWrite(7, LOW);
+    digitalWrite(13, !LOW);
   }
-
-  delay(1000);
+  delay(5000);
 }
